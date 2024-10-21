810d57fca14e1929b5a887969f363ba9db68bce0
Floris Nzabakira <nzafloris@gmail.com>
2024-10-21 17:02:27 +0200

    adding pip installation setup

---
+++ env_setup/install_env_pip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install necessary pip packages from requirements file
+pip install -r ilan_env_requirements.txt
+pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+
+# Set up environment variable scripts
+mkdir -p /etc/pip/activate.d
+mkdir -p /etc/pip/deactivate.d
+touch /etc/pip/activate.d/env_vars.sh
+touch /etc/pip/deactivate.d/env_vars.sh
+
+# Add environment variables for activation
+echo '#!/bin/sh' >> /etc/pip/activate.d/env_vars.sh
+echo 'export OLD_LD_LIBRARY_PATH=${LD_LIBRARY_PATH}' >> /etc/pip/activate.d/env_vars.sh
+echo 'export LD_LIBRARY_PATH=/usr/local/lib/:${OLD_LD_LIBRARY_PATH}' >> /etc/pip/activate.d/env_vars.sh
+
+# Add environment variables for deactivation
+echo '#!/bin/sh' >> /etc/pip/deactivate.d/env_vars.sh
+echo 'export LD_LIBRARY_PATH=${OLD_LD_LIBRARY_PATH}' >> /etc/pip/deactivate.d/env_vars.sh
+echo 'unset OLD_LD_LIBRARY_PATH' >> /etc/pip/deactivate.d/env_vars.sh
